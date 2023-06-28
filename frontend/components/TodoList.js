2def4e98783e3764bf88ff64613f6c926c117175
clsCA-86 <123132398+clsCA-86@users.noreply.github.com>
2023-06-28 13:35:09 -0700

    done

---
+++ frontend/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {
+          this.props.todos.map(todo => {
+            return (<Todo handleToggle={this.props.handleToggle} todo={todo} />)
+          }
+          )
+        }
+      </ul>
+    )
+  }
+}
